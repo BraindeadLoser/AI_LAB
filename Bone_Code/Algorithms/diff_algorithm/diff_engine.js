b689ee24e6815a1b9eb0561460bcc82d75ab4656
BraindeadLoser <orangeaugust830.com>
2026-06-27 13:15:52 +0530

    auto: sync changes at 2026-06-27 13:15:52

--- Bone_Code/Algorithms/diff_algorithm/diff_engine.js
+++ Bone_Code/Algorithms/diff_algorithm/diff_engine.js
@@ -1,5 +1,5 @@
 class DiffEngine {
-  computeLCS(oldLines, newLines) {
+  computeLCSMatrix(oldLines, newLines) {
     const rows = oldLines.length + 1;
     const cols = newLines.length + 1;
     const matrix = [];
@@ -24,46 +24,77 @@ class DiffEngine {
     return matrix;
   }
 
-  computeDiffOperations(oldLines, newLines) {
-    const maxLines = Math.max(oldLines.length, newLines.length);
-    const operations = [];
+computeDiffOperations(oldLines, newLines) {
+  const matrix = this.computeLCSMatrix(oldLines, newLines);
 
-    for (let i = 0; i < maxLines; i++) {
-      const oldLine = oldLines[i];
-      const newLine = newLines[i];
+  let i = oldLines.length;
+  let j = newLines.length;
 
-      if (oldLine === newLine) {
-        if (oldLine !== undefined) {
-          operations.push({
-            type: "unchanged",
-            oldLine: i + 1,
-            newLine: i + 1,
-            text: oldLine
-          });
-        }
-      } else {
-        if (oldLine !== undefined) {
-          operations.push({
-            type: "removed",
-            oldLine: i + 1,
-            newLine: null,
-            text: oldLine
-          });
-        }
+  const operations = [];
 
-        if (newLine !== undefined) {
-          operations.push({
-            type: "added",
-            oldLine: null,
-            newLine: i + 1,
-            text: newLine
-          });
-        }
-      }
+  while (i > 0 && j > 0) {
+    const oldText = (oldLines[i - 1] || "").replace(/\r/g, "");
+    const newText = (newLines[j - 1] || "").replace(/\r/g, "");
+
+    if (oldText === newText) {
+      operations.push({
+        type: "unchanged",
+        oldLine: i,
+        newLine: j,
+        text: oldLines[i - 1]
+      });
+
+      i--;
+      j--;
     }
+    else if (matrix[i - 1][j] >= matrix[i][j - 1]) {
+      operations.push({
+        type: "removed",
+        oldLine: i,
+        newLine: null,
+        text: oldLines[i - 1]
+      });
 
-    return operations;
+      i--;
+    }
+    else {
+      operations.push({
+        type: "added",
+        oldLine: null,
+        newLine: j,
+        text: newLines[j - 1]
+      });
+
+      j--;
+    }
   }
+
+  while (i > 0) {
+    operations.push({
+      type: "removed",
+      oldLine: i,
+      newLine: null,
+      text: oldLines[i - 1]
+    });
+
+    i--;
+  }
+
+  while (j > 0) {
+    operations.push({
+      type: "added",
+      oldLine: null,
+      newLine: j,
+      text: newLines[j - 1]
+    });
+
+    j--;
+  }
+
+  operations.reverse();
+
+  return operations;
+}
 
   buildDiffModel(originalContent, patchedContent) {
     const oldLines = (originalContent || "").split("\n");
